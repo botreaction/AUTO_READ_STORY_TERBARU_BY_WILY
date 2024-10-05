1b92e8beadbb1fdc8039e43bfd0682c77c428c8b
wily <103911498+botreaction@users.noreply.github.com>
2024-10-05 13:55:44 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+// settings.js
+const settings = {
+  autoReadStory: true, // Default: Auto baca cerita (dasar) diaktifkan
+  autoReadStoryEmoji: true, // Default: Auto baca cerita dengan emoji diaktifkan
+  autoOnline: true, // Default: Auto online diaktifkan
+  storyReadInterval: 1000 // Default: Baca cerita setiap 1 detik (dalam milidetik)
+};
+
+module.exports = settings;
